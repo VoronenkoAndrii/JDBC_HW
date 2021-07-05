071a4232e16a82f4b793a739a075eb6adf2319a9
Voronenko Andrii <83990569+VoronenkoAndrii@users.noreply.github.com>
2021-07-05 15:39:46 +0300

    Add files via upload

--- src/test/java/DataBaseTest.java
+++ src/test/java/DataBaseTest.java
@@ -11,11 +11,13 @@ public class DataBaseTest {
     public static void main(String[] args) throws SQLException {
         DataBaseTest dataBaseManipulations = new DataBaseTest();
         //   dataBaseManipulations.createDataBase();
-
         // dataBaseManipulations.createTable();
-      //  dataBaseManipulations.insertTable();
-       // dataBaseManipulations.updateTable();
-        dataBaseManipulations.assertToUpdate();
+        //  dataBaseManipulations.insertTable();
+        // dataBaseManipulations.updateTable();
+        //  dataBaseManipulations.assertToUpdate();
+        //  dataBaseManipulations.deleteRowInTable();
+        //  dataBaseManipulations.dropTable();
+        dataBaseManipulations.dropDatabase();
     }
 
     static final String user = "root";
@@ -24,7 +26,7 @@ public class DataBaseTest {
 
     public void createDataBase() {
         Connection conn = null;
-        String url = "jdbc:mysql://127.0.0.1:3306/test1?serverTimezone=UTC";
+        String url = "jdbc:mysql://127.0.0.1:3306/test2?serverTimezone=UTC";
 
         try {
             conn = DriverManager.getConnection(url1, user, pass);
@@ -122,20 +124,20 @@ public class DataBaseTest {
     public void updateTable() throws SQLException {
         Connection conn = null;
         Statement stmt = null;
-        String url = "jdbc:mysql://127.0.0.1:3306/test1?serverTimezone=UTC";
+        String url = "jdbc:mysql://127.0.0.1:3306/test2?serverTimezone=UTC";
 
         try {
             conn = DriverManager.getConnection(url, user, pass);
             if (conn != null) {
                 System.out.println("Connected to the database test1");
-                stmt  = conn.createStatement();
+                stmt = conn.createStatement();
             }
             String updateRow = "UPDATE `test2`.`REGISTRATION` " +
                     "SET `age` = '44' " +
                     "WHERE (`id` = '2')";
             if (stmt != null) {
                 stmt.execute(updateRow);
-            }else{
+            } else {
                 System.out.println("stmt creation error");
             }
             System.out.println("age is updated");
@@ -150,52 +152,139 @@ public class DataBaseTest {
             }
         }
     }
-public void assertToUpdate() throws SQLException {
-    Connection conn ;
-    Statement stmt = null;
-    ResultSet resultSet = null;
-    String url = "jdbc:mysql://127.0.0.1:3306/test1?serverTimezone=UTC";
-    conn = DriverManager.getConnection(url, user, pass);
-    System.out.println("Connection driver created");
 
-    if (conn != null) {
-        stmt = conn.createStatement();
-        System.out.println("Statement created");
-    }
-    if (conn != null) {
-        resultSet = stmt.executeQuery("select * from test2.REGISTRATION");
-        System.out.println("Execute query");
-    }
-    List<String[]> allRows = new ArrayList<>();
-    int numberColumns = 4;
-    if (resultSet != null) {
-        System.out.println("founded records");
-        while (resultSet.next()) {
-            String[] currentRow = new String[numberColumns];
-            for (int i = 1; i <= numberColumns; i++) {
-                currentRow[i - 1] = resultSet.getString(i);
-            }
-            allRows.add(currentRow);
+    public void assertToUpdate() throws SQLException {
+        Connection conn;
+        Statement stmt = null;
+        ResultSet resultSet = null;
+        String url = "jdbc:mysql://127.0.0.1:3306/test2?serverTimezone=UTC";
+        conn = DriverManager.getConnection(url, user, pass);
+        System.out.println("Connection driver created");
+
+        if (conn != null) {
+            stmt = conn.createStatement();
+            System.out.println("Statement created");
         }
-    }else{
-        System.out.println("No records found");
+        if (conn != null) {
+            resultSet = stmt.executeQuery("select * from test2.REGISTRATION");
+            System.out.println("Execute query");
+        }
+        List<String[]> allRows = new ArrayList<>();
+        int numberColumns = 4;
+        if (resultSet != null) {
+            System.out.println("founded records");
+            while (resultSet.next()) {
+                String[] currentRow = new String[numberColumns];
+                for (int i = 1; i <= numberColumns; i++) {
+                    currentRow[i - 1] = resultSet.getString(i);
+                }
+                allRows.add(currentRow);
+            }
+        } else {
+            System.out.println("No records found");
+        }
+
+        boolean iscontains = false;
+        for (String[] row : allRows) {
+            if (row[0].equals("2")) {
+                if (row[3].equals("44")) {
+                    System.out.println("row[3]." + row[3]);
+                    iscontains = true;
+                }
+            }
+        }
+        assertTrue(iscontains);
+
+        conn.close();
+        stmt.close();
     }
 
-    boolean iscontains = false;
-    for(String[] row:allRows){
-        if(row[0].equals("2")){
-            if(row[3].equals("44")){
-                System.out.println("row[3]."+row[3]);
-                iscontains = true;
+    public void deleteRowInTable() throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String url = "jdbc:mysql://127.0.0.1:3306/test2?serverTimezone=UTC";
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+            if (conn != null) {
+                System.out.println("Connected to the database test1");
+                stmt = conn.createStatement();
+            }
+            String updateRow = "UPDATE `test2`.`REGISTRATION` " +
+                    "SET `age` = '44' " +
+                    "WHERE (`id` = '2')";
+            if (stmt != null) {
+                stmt.execute(updateRow);
+            }
+
+            String sqlInsertStatement = "DELETE FROM `test2`.`REGISTRATION` WHERE (`id` = '1')";
+            if (stmt != null) {
+                stmt.execute(sqlInsertStatement);
+            }
+            System.out.println("Row 2 was deleted");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+            if (stmt != null) {
+                stmt.close();
             }
         }
     }
-    assertTrue(iscontains);
 
-    conn.close();
-    stmt.close();
+    public void dropTable() throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String url = "jdbc:mysql://127.0.0.1:3306/test2?serverTimezone=UTC";
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+            if (conn != null) {
+                System.out.println("Connected to the database test1");
+                stmt = conn.createStatement();
+            }
+            stmt.executeUpdate("DROP TABLE REGISTRATION");
+            DatabaseMetaData databaseMetaData = conn.getMetaData();
+            ResultSet resultSet = databaseMetaData.getTables(null, null, "REGISTRATION", null);
+            assertTrue(resultSet != null);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public void dropDatabase() throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String url = "jdbc:mysql://127.0.0.1:3306/test1?serverTimezone=UTC";
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+            if (conn != null) {
+                System.out.println("Connected to the database test1");
+                stmt = conn.createStatement();
+            }
+            stmt.executeUpdate("DROP DATABASE test2");
+            assertTrue(stmt != null);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
 }
-}
+
+
 
 
 
